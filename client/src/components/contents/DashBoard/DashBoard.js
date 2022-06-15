7b258ac2c16025cf6e8f55d8d98eb8d6a9bf2b96
NhatTan-uit <19522171@gm.uit.edu.vn>
2022-06-16 03:28:38 +0700

    commit by k

--- client/src/components/contents/DashBoard/DashBoard.js
+++ client/src/components/contents/DashBoard/DashBoard.js
@@ -2,43 +2,63 @@ import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import CartHistory from './CartHistory/CartHistory';
 import UserChangePass from './UserChangePass/UserChangePass';
+import CameraAltOutlinedIcon from '@mui/icons-material/CameraAltOutlined';
 
 function DashBoard() {
   const location = useLocation();
   const [hoverstate, setHoverState] = useState();
+  const [imgstate, setImgState] = useState('dashboard__user__image');
 
   const handleAppear = () => {
     setHoverState(true);
+    setImgState('dashboard__user__image__hover');
   }
 
   const handleHide = () => {
     setHoverState(false);
+    setImgState('dashboard__user__image');
   }
 
+  const handleChangeUserImage = () => {
+    alert("Image is under changing");
+  }
+  
   console.log(location);
 
   return (
     <div className="mainpage__background__color">
       <div className='mainpage'>
         <div className="mainpage__header">
-          <div>
+          <div className='dashboard__user__name'>
             <h2>Hello {location.state.name}</h2>
-            {location.state.userImage ?
-              <div className='dashboard__user__image' onMouseOver={handleAppear} onMouseOut={handleHide}>
-                <img src={`/uploads/${location.state.userImage}`} alt='...' />
-              </div> :
-              <div className='dashboard__user__image' onMouseOver={handleAppear} onMouseOut={handleHide}>
-                <img src={`/uploads/nonuser.png`} alt='...' />
-              </div>}
-            {hoverstate && <div>File chooser</div>}
-            {location.state.aboutme ?
-              <div>
-                {location.state.aboutme}
-              </div> :
-              <div>
-                Hello the the world
-              </div>}
           </div>
+          {location.state.userImage ?
+            <img
+              className={imgstate}
+              onMouseOver={handleAppear}
+              onMouseOut={handleHide}
+              src={`/uploads/${location.state.userImage}`}
+              alt='...'
+            />
+            :
+            <img
+              className={imgstate}
+              onMouseOver={handleAppear}
+              onMouseOut={handleHide}
+              src={`/uploads/nonuser.png`}
+              alt='...'
+            />}
+          {hoverstate &&
+            <div onMouseOver={handleAppear} onMouseOut={handleAppear} className='dashboard__user__image__file'>
+              <CameraAltOutlinedIcon fontSize='large' onClick={handleChangeUserImage}/>
+            </div>}
+          {location.state.aboutme ?
+            <div>
+              {location.state.aboutme}
+            </div> :
+            <div>
+              Hello the the world
+            </div>}
         </div>
         <div className="mainpage__container">
           <div className="mainpage__col__big flex__order__second">
